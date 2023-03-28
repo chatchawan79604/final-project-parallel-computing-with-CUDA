9b2d0fea0958b08fa144e3ae8bef4cc8c99ae99e
napatswift <napat.do@ku.th>
2023-03-29 00:47:15 +0700

    Create functions

--- seq.c
+++ seq.c
@@ -44,6 +44,49 @@ void frequency_count(int *corpus, int corpus_size, int max_len, int **count_arr)
   }
 }
 
+void term_frequency(int **term_counts, int corpus_size, int vocab_size, double **out_arr)
+{
+  for (int i = 0; i < corpus_size; i++)
+  {
+    int wc_sum = 0;
+    for (size_t wj = 0; wj < vocab_size; wj++)
+    {
+      wc_sum += term_counts[i][wj];
+    }
+
+    for (size_t j = 0; j < vocab_size; j++)
+    {
+      out_arr[i][j] = (double)term_counts[i][j] / wc_sum;
+    }
+  }
+}
+
+void invert_document_frequency(int **term_counts, int corpus_size, int vocab_size, int smooth_idf, double *out_arr)
+{
+  for (size_t wi = 0; wi < vocab_size; wi++)
+  {
+    int w_dc = 0 + smooth_idf;
+    for (size_t di = 0; di < corpus_size; di++)
+    {
+      if (term_counts[di][wi])
+        w_dc++;
+    }
+
+    out_arr[wi] = log((double)corpus_size / w_dc) + 1;
+  }
+}
+
+void tfidf(double** tf_arr, double *idf_arr, double **out_arr, int corpus_size, int vocab_size)
+{
+  for (int di = 0; di < corpus_size; di++)
+  {
+    for (size_t wi = 0; wi < vocab_size; wi++)
+    {
+      out_arr[di][wi] = tf_arr[di][wi] * idf_arr[wi];
+    }
+  }
+}
+
 void read_corpus(char *filename, int **array, int *num_rows, int *num_cols)
 {
   FILE *fp;
@@ -64,8 +107,6 @@ void read_corpus(char *filename, int **array, int *num_rows, int *num_cols)
   {
     num_columns = 0;
     num_lines++;
-
-    // count the number of columns in the current line
     char *token = strtok(line, " ");
     while (token != NULL)
     {
@@ -73,27 +114,17 @@ void read_corpus(char *filename, int **array, int *num_rows, int *num_cols)
         num_columns++;
       token = strtok(NULL, " ");
     }
-
-    // update the maximum number of columns seen so far
     if (num_columns > *num_cols)
     {
       *num_cols = num_columns;
     }
-
-    // count non-empty lines
     if (num_columns > 0)
     {
       (*num_rows)++;
     }
   }
-
-  // Allocate memory for the 2D array
   *array = (int *)malloc(*num_rows * *num_cols * sizeof(int));
-
-  // Reset the file pointer to the beginning of the file
   fseek(fp, 0, SEEK_SET);
-
-  // Read the integers in the file into the 2D array
   int i = 0, j;
   while (fgets(line, line_size, fp) != NULL)
   {
@@ -121,110 +152,50 @@ int main()
   int smooth_idf = 0; // should add 1 or not
   int vocab_size = 0, seq_max_len = 0, corpus_size = 0;
   int *corpus;
+
+  // read input corpus from text file
   read_corpus("corpus_large.txt", &corpus, &corpus_size, &seq_max_len);
   printf("seq_max_len: %d, corpus size: %d\n", seq_max_len, corpus_size);
 
-  // printf("input corpus\n");
-  // for (size_t i = 0; i < corpus_size; i++)
-  // {
-  //   for (size_t j = 0; j < seq_max_len; j++)
-  //   {
-  //     printf("% 3d ", corpus[i * seq_max_len + j]);
-  //   }
-  //   printf("\n");
-  // }
-
+  // find maximum word id
   vocab_size = find_vocab_size(corpus, corpus_size, seq_max_len);
   printf("vocab size: %d\n", vocab_size);
 
+  // initailize word counts array
   int **counts = (int **)malloc(sizeof(int *) * corpus_size);
   for (int i = 0; i < corpus_size; i++)
   {
     counts[i] = (int *)malloc(sizeof(int) * vocab_size);
     for (size_t j = 0; j < vocab_size; j++)
     {
+      // set to identity
       counts[i][j] = 0;
     }
   }
 
-  frequency_count(corpus, corpus_size, seq_max_len, counts);
-
-  // printf("word counts in each doc:\n");
-  // for (int i = 0; i < corpus_size; i++)
-  // {
-  //   for (size_t j = 0; j < vocab_size; j++)
-  //   {
-  //     printf("%d, ", counts[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  // printf("\n");
-
+  // initailize tf array
   double **tf_arr = (double **)malloc(sizeof(double *) * corpus_size);
   for (int i = 0; i < corpus_size; i++)
   {
-    int wc_sum = 0;
-    for (size_t wj = 0; wj < vocab_size; wj++)
-    {
-      wc_sum += counts[i][wj];
-    }
-
     tf_arr[i] = (double *)malloc(sizeof(double) * vocab_size);
-    for (size_t j = 0; j < vocab_size; j++)
-    {
-      tf_arr[i][j] = (double)counts[i][j] / wc_sum;
-    }
   }
 
-  // printf("tf:\n");
-  // for (int i = 0; i < corpus_size; i++)
-  // {
-  //   for (size_t j = 0; j < vocab_size; j++)
-  //   {
-  //     printf("%.2f, ", tf_arr[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  // printf("\n");
-
+  // initailize idf array
   double *idf_arr = (double *)malloc(sizeof(double) * vocab_size);
-  for (size_t wi = 0; wi < vocab_size; wi++)
+
+  frequency_count(corpus, corpus_size, seq_max_len, counts);
+  term_frequency(counts, corpus_size, vocab_size, tf_arr);
+  invert_document_frequency(counts, corpus_size, vocab_size, smooth_idf, idf_arr);
+  tfidf(tf_arr, idf_arr, tf_arr, corpus_size, vocab_size);
+
+  free(idf_arr);
+  for (int i = 0; i < corpus_size; i++)
   {
-    int w_dc = 0 + smooth_idf;
-    for (size_t di = 0; di < corpus_size; di++)
-    {
-      if (counts[di][wi])
-        w_dc++;
-    }
-
-    idf_arr[wi] = log((double)corpus_size / w_dc) + 1;
+    free(tf_arr[i]);
+    free(counts[i]);
   }
-
-  // printf("idf:\n");
-  // for (size_t wi = 0; wi < vocab_size; wi++)
-  // {
-  //   printf("%.3f, ", idf_arr[wi]);
-  // }
-  // printf("\n\n");
-
-  double **tf_idf_arr = tf_arr;
-  for (int di = 0; di < corpus_size; di++)
-  {
-    for (size_t wi = 0; wi < vocab_size; wi++)
-    {
-      tf_idf_arr[di][wi] *= idf_arr[wi];
-    }
-  }
-
-  // printf("tf-idf:\n");
-  // for (int i = 0; i < corpus_size; i++)
-  // {
-  //   for (size_t j = 0; j < vocab_size; j++)
-  //   {
-  //     printf("%.2f, ", tf_idf_arr[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  // printf("\n");
+  free(tf_arr);
+  free(counts);
+  free(corpus);
   return 0;
 }
